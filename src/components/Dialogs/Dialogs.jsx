ff947fea73043f4dad8c936141487f9e6931f31f
NikitaKS <pancxxx2@gmail.com>
2019-11-07 20:35:16 +0100

    22: Dialogs Route

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-const Dialogs = () =>{
-  return (
-      <div className={s.dialogs}>
-          <div className={s.dialogsItems}>
-            <div className={`${s.item} ${s.active}`}>Dima</div>
-            <div className={s.item}>Sveta</div>
-            <div className={s.item}>Ola</div>
-          </div>
-          <div className={s.massages}>
-              <div className={s.massage}>Hi</div>
-              <div className={s.massage}>How are u?</div>
-              <div className={s.massage}>Good</div>
-          </div>
-      </div>
-  )
+import {NavLink} from "react-router-dom";
+
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.item} ${s.active}`}>
+                    <NavLink to="/dialogs/1">Dima</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/2">Sveta</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/3">Andrey</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/4">Oleg</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/5">Zheka</NavLink>
+                </div>
+            </div>
+            <div className={s.massages}>
+                <div className={s.massage}>Hi</div>
+                <div className={s.massage}>How are u?</div>
+                <div className={s.massage}>Good</div>
+            </div>
+        </div>
+    )
 };
 
 export default Dialogs;
