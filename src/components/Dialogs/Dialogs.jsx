caa32dabfc5739adf675ac98b6036e13c3363e9f
NikitaKS <pancxxx2@gmail.com>
2019-11-07 19:28:52 +0100

    21: dialogs вёрстка

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import s from './Dialogs.module.css';
 const Dialogs = () =>{
   return (
-      <div>
-          Dialogs
+      <div className={s.dialogs}>
+          <div className={s.dialogsItems}>
+            <div className={`${s.item} ${s.active}`}>Dima</div>
+            <div className={s.item}>Sveta</div>
+            <div className={s.item}>Ola</div>
+          </div>
+          <div className={s.massages}>
+              <div className={s.massage}>Hi</div>
+              <div className={s.massage}>How are u?</div>
+              <div className={s.massage}>Good</div>
+          </div>
       </div>
   )
 };
